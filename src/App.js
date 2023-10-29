46643154123ab02db283ee7e666c1288242f13be
kuror0lucifer <kovalishin.2014@mail.ru>
2023-10-29 16:56:25 +0200

    bug fixed

--- src/App.js
+++ src/App.js
@@ -2,23 +2,7 @@ import React, { useState } from "react";
 import Block from "./components/Block";
 import CreateField from "./components/СreateField";
 
-const todo = [
-  {
-    id: 1,
-    title: "Buy new sweatshirt",
-    isCompleted: false,
-  },
-  {
-    id: 2,
-    title: "Begin promotional phase",
-    isCompleted: false,
-  },
-  {
-    id: 3,
-    title: "Read an article",
-    isCompleted: false,
-  },
-];
+const todo = [];
 
 export default function App() {
   const [todos, setTodos] = useState(todo);
--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -12,7 +12,7 @@ export default function ListItems({todo, onChange, onDel}) {
                 <div className='flex justify-between items-center mb-6 first-of-type:mt-8'>
                     <CheckBox onChange={onChange} todo={e}/>
                     <div className='w-80 h-6 border-b mr-3'>
-                    <li key={e.id} className={`${e.isCompleted ? 'line-through' : ''}`}>{e.title}</li>
+                    <li key={e.id} className={`${e.isCompleted ? 'line-through' : ''} overflow-clip`}>{e.title}</li>
                     </div>
                     <BiTrashAlt onClick={() => onDel(e.id)} className='w-5 h-5 cursor-pointer hover:text-red-500'/>
                 </div>
